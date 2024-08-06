0f5beb04d2c15534b2f415238eeadfb7afbcf18e
yashighokar1412 <165381744+yashighokar1412@users.noreply.github.com>
2024-08-06 16:43:45 +0530

    Update AppTest.java

--- src/test/java/com/example/todo/AppTest.java
+++ src/test/java/com/example/todo/AppTest.java
@@ -2,15 +2,18 @@ package com.example.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class TodoApplicationTests {
 
+    @MockBean
+    private SomeService someService;
+
     @Test
     void contextLoads() {
         assertTrue(true);
     }
 }
-
